4706de865ef8ca159a567e7251dd3af5b40d3629
sefa kapısız <sefa.kapisiz@coino.com>
2022-10-16 00:33:35 +0300

    upd

--- SignalRSample/wwwroot/js/usersCount.js
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -2,7 +2,8 @@
 //configure to TransportType with help of adding comma next to url and => ",signalR.HttpTransportType.LongPolling/ServerSentEvents/WebSockets(Default)"
 var connectionUserCount = new signalR.HubConnectionBuilder()
     //.configureLogging(signalR.LogLevel.Information)       if we want to logging we can use this line of code
-    .withUrl("/hubs/userCount",signalR.HttpTransportType.WebSockets).build();
+    .withAutomaticReconnect()
+    .withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets).build();
 
 //connect to methods that hub invokes aka receive notifications from hub
 connectionUserCount.on("updateTotalViews", (value) => {
@@ -19,7 +20,7 @@ connectionUserCount.on("updateTotalUsers", (value) => {
 function newWindowLoadedOnClient() {
     //if we use send we don't expect any response but when we use invoke we do
     //connectionUserCount.Send("NewWindowLoaded");
-    connectionUserCount.invoke("NewWindowLoaded","Sefa").then((value) => console.log(value));
+    connectionUserCount.invoke("NewWindowLoaded", "Sefa").then((value) => console.log(value));
 }
 
 //start connection
@@ -32,5 +33,17 @@ function fulfilled() {
 function rejected() {
     //rejected logs
 }
+
+connectionUserCount.onclose((error) => {
+    toastr.error("Reconnection is failed..");
+});
+
+connectionUserCount.onreconnected((connectionId) => {
+    toastr.success("Reconnected");
+});
+
+connectionUserCount.onreconnecting((error) => {
+    toastr.warning("Reconnecting...");
+});
 
 connectionUserCount.start().then(fulfilled, rejected);
