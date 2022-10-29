e82c9122d0d82583f66417e90f1917776eb06dca
sefa kapısız <sefa.kapisiz@coino.com>
2022-10-29 22:09:24 +0300

    upd

---
+++ SignalRSampleReWrite/Hubs/UserCountHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSampleReWrite.Hubs
+{
+    public class UserCountHub : Hub
+    {
+        private int ViewsCount;
+        private int ConnectedUsers;
+
+        public UserCountHub(int userCount, int connectedUsers)
+        {
+            ViewsCount = userCount;
+            ConnectedUsers = connectedUsers;
+        }
+
+        public async Task SendNotification()
+        {
+            ViewsCount++;
+            await Clients.All.SendAsync("ReceiveNotification", ViewsCount);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            ConnectedUsers++;
+            await Clients.All.SendAsync("UserConnected", ConnectedUsers);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            ConnectedUsers--;
+            await Clients.All.SendAsync("UserDisconnected", ConnectedUsers);
+        }
+    }
+}
---
+++ SignalRSampleReWrite/wwwroot/js/usersCount.js
@@ -0,0 +1,21 @@
+﻿var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/usersCount")
+    .build();
+
+var totalViewsCounter = document.getElementById("totalViewsCounter");
+var totalUsersCounter = document.getElementById("totalUsersCounter");
+
+connection.on("ReceiveNotification", value => {
+
+    totalViewsCounter.innerText = value.toString();
+});
+
+connection.on("UserConnected", value => {
+    totalUsersCounter.innerText = value.toString();
+});
+
+connection.on("UserDisconnected", value => {
+    totalUsersCounter.innerText = value.toString();
+});
+
+connection.start();
