c81bdd5c8377b4ea3d4d7ac73c92fa7fdc889007
Muhammet Sefa Kapısız <62629437+sefakpsz@users.noreply.github.com>
2023-09-13 17:31:47 +0300

    Delete SignalRSampleReWrite directory

--- SignalRSampleReWrite/Hubs/OrderHub.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.SignalR;
-
-namespace SignalRSampleReWrite.Hubs
-{
-    public class OrderHub : Hub
-    {
-    }
-}
--- SignalRSampleReWrite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SignalRSampleReWrite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
